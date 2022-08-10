2846c23f0123591a235aeb9e9176a9c9332e5eac
ahmedadel56 <ahmdadl563@gmail.com>
2022-08-10 23:35:27 +0200

    Add some tests in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+@counter = 1
+
+5.times do 
+    User.create(name: "user  #{@counter}") 
+    Hotel.create(name: "hotel #{@counter}", 
+        description:"description #{@counter}",
+        cost: @counter , address: "address #{@counter}",
+        image: ["image #{@counter}","image #{@counter+1}"],
+    user_id:User.first.id)
+    Reservation.create(city: "city #{@counter}", 
+        date: Date.new,
+        number_of_days: @counter , number_of_rooms: @counter,
+        number_of_guests:@counter, user_id:User.first.id,
+    hotel_id: Hotel.first.id)
+    @counter += 1
+end
