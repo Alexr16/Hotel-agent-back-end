a60ff6ab4bdb55598808f63c64beca3d0debd5a1
ahmedadel56 <ahmdadl563@gmail.com>
2022-08-10 23:17:57 +0200

    Add model associations

--- db/migrate/20220810201658_create_hotels.rb
+++ db/migrate/20220810201658_create_hotels.rb
@@ -6,7 +6,7 @@ class CreateHotels < ActiveRecord::Migration[7.0]
       t.integer :cost
       t.string :address
       t.string :image, array: true, default: []
-
+      t.references :user, null: true, foreign_key: true 
       t.timestamps
     end
   end
--- db/migrate/20220810202715_create_reservations.rb
+++ db/migrate/20220810202715_create_reservations.rb
@@ -6,7 +6,8 @@ class CreateReservations < ActiveRecord::Migration[7.0]
       t.integer :number_of_days
       t.integer :number_of_rooms
       t.integer :number_of_guests
-
+      t.references :user, null: true, foreign_key: true 
+      t.references :hotel, null: true, foreign_key: true 
       t.timestamps
     end
   end
