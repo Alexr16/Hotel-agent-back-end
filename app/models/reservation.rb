ad6f7dd138d45df14b23fb83f90452f1ad570a0d
ahmedadel56 <ahmdadl563@gmail.com>
2022-08-10 23:59:09 +0200

    Fix linter errors

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :hotel
   belongs_to :user
-  validates :city, presence: true, length: { minimum: 3, maximum: 250 } 
+  validates :city, presence: true, length: { minimum: 3, maximum: 250 }
   validates :date, presence: true
   validates :number_of_days, :number_of_rooms, :number_of_guests, numericality: { greater_than: 0, only_integer: true }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-    
   has_many :hotels
   has_many :reservations
-  validates :name, presence: true, length: { minimum: 3, maximum: 250 } , dependent: :destroy
+  validates :name, presence: true, length: { minimum: 3, maximum: 250 }, dependent: :destroy
 end
